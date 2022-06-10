de066710a4540e832c70728ebaf6088dd180cf34
MRW Neundorf <ninevillage@gmail.com>
2022-06-11 00:31:20 +0200

    feat(cli-greeting): init new cargo bin (#6)
    Implemented simple greeting cli.

---
+++ cli-greeting/src/main.rs
@@ -0,0 +1,11 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        println!("Hello, {}", args[1]);
+    } else {
+        println!("Hello, John")
+    }
+}
