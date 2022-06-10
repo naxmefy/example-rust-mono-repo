72b5c4d5224e7989edda903b47309cfa5eb3b46d
MRW Neundorf <ninevillage@gmail.com>
2022-06-11 00:30:09 +0200

    feat(cli-greeting): implement simple greeting

--- cli-greeting/src/main.rs
+++ cli-greeting/src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        println!("Hello, {}", args[1]);
+    } else {
+        println!("Hello, John")
+    }
 }
